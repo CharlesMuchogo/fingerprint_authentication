45554ee3af27abc77eb37b2536a564531e2c150c
charlesMuchogo <muchpaul2@gmail.com>
2022-10-28 13:04:51 +0300

    added a homepage

--- lib/homepage/home.dart
+++ lib/homepage/home.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,12 +11,40 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  double progress = 0;
+
+  late InAppWebViewController controler;
+  GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Homepage"),
+      key: _key,
+      appBar: AppBar(
+        title: Text("Homepage"),
+      ),
+      body: Stack(children: [
+        InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: Uri.parse("https://www.youtube.com/"),
+          ),
+          onWebViewCreated: (InAppWebViewController _controller) {
+            controler = _controller;
+          },
+          onProgressChanged:
+              (InAppWebViewController _controller, int _progress) {
+            setState(() {
+              progress = _progress / 100;
+            });
+          },
         ),
-        body: Center(child: Text("Welcome to homepage")));
+        progress < 1
+            ? SizedBox(
+                child: LinearProgressIndicator(
+                  value: progress,
+                ),
+              )
+            : SizedBox()
+      ]),
+    );
   }
 }
